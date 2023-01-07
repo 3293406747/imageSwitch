f400612d6d8c622e165b74a84164f928e1ee652b
py85329140 <3293406747@qq.com>
2023-01-08 01:28:29 +0800

    u

--- imageSwitch.py
+++ imageSwitch.py
@@ -2,6 +2,7 @@ import sys
 import time, os
 import click
 import pyperclip
+from io import BytesIO
 from PIL import Image, ImageGrab
 import base64
 from loguru import logger
@@ -19,9 +20,10 @@ logger.add(
 
 class ImageSwitch:
 	""" 剪切板图片自动转为链接或base64 默认转为链接 """
+
 	def __init__(self):
-		self.__IMAGE_NAME = "im.png"
 		self.__imageFlag = "link"
+
 	@staticmethod
 	def getIm():
 		""" 获取剪切板图片 """
@@ -37,28 +39,27 @@ class ImageSwitch:
 		""" base64编码 """
 		return base64.b64encode(target).decode()
 
-	def switch(self,im,choice):
+	def switch(self, im, choice):
 		""" 图像转换link或base64 """
-		im.save(self.__IMAGE_NAME)
-		with open(self.__IMAGE_NAME, "rb") as f:
-			s = self.b64encode(f.read())
-			if choice == "link":  # 转为链接
-				s = "data:image/png;base64," + s
-			elif choice == "base64":  # 转为base64
-				self.__imageFlag = "base64"
-			else:
-				raise ValueError
-		os.remove(self.__IMAGE_NAME)
+		io = BytesIO()
+		im.save(io, format="PNG")
+		s = self.b64encode(io.getvalue())
+		if choice == "link":  # 转为链接
+			s = "data:image/png;base64," + s
+		elif choice == "base64":  # 转为base64
+			self.__imageFlag = "base64"
+		else:
+			raise ValueError
 		return s
 
 	def main(self, choice="link"):
 		print("******剪切板图片转link工具******")
 		logger.info("> 程序开始运行>>>>>>>>>>>>>>>>")
 		while True:
-			im = self.getIm()	# 获取剪切板图片
+			im = self.getIm()  # 获取剪切板图片
 			if isinstance(im, Image.Image):
-				s = self.switch(im,choice)	# 图像转换link或base64
-				self.copy(s)		# 复制到剪切板
+				s = self.switch(im, choice)  # 图像转换link或base64
+				self.copy(s)  # 复制到剪切板
 				logger.info(f"剪切板图片转{self.__imageFlag}成功!!!")
 			time.sleep(2)
 
